84a7736a566bdd519926b8c534ef145bbf4480e4
N0thing <91046743+judit201@users.noreply.github.com>
2021-10-25 20:02:30 +0700

    Tugas 2 Praktikum 5

---
+++ AriefWiraJudithantyo-064002100027-Praktikum-5-Tugas2.py
@@ -0,0 +1,35 @@
+total = 0
+
+print(38*"=")
+print("Selamat datang di kebun binatang Bogor")
+print(38*"=")
+
+while True:
+    try:
+        umur = int(input("Masukkan umur : "))
+    except ValueError:
+        break
+    if umur <= 2:
+            print("Gratis")
+    elif umur in [3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
+            total += 14.00
+            print("Harga $14.00")
+            print(f"Running total : ${total:.2f}")
+    elif umur >= 65:
+            total += 18.00
+            print("Harga $18.00")
+            print(f"Running total : ${total:.2f}")
+    else:
+            total += 23.00
+            print("Harga $23.00")
+            print(f"Running total : ${total:.2f}")
+            
+uang = float(input("Masukkan jumlah uang : "))
+if uang > total:
+    bayar = uang - total
+    print(f"Running kembalian : ${bayar:.2f}")
+elif uang < total:
+    bayar = total - uang
+    print(f"Maaf uang anda kurang ${bayar:.2f}")
+else:
+    print("Uang anda pas, Terimakasih!")
